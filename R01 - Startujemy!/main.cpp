a17f19c0aee8d8ab4166c07474f4ccfdadbf2ca8
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-04-29 01:42:30 +0200

    R01 done

---
+++ R01 - Startujemy!/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Deklaracja - informujemy czym jest nazwa stopy w programie
+    extern int stopy1;
+
+    //Definicja - rezerwujemy w pamięci miejsce na nowy obiekt typu całkowiteg (int) o nazwie "stopy"
+    int stopy2;
+
+    //Definicja jest równocześnie deklaracją
+    //Deklaracja nie jest równocześnie definicją
+
+    /*
+    Dłuższy komenatrz
+    */
+}
