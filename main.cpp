8f3a18994edcc34fc3353b0e286ff932d37de3eb
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-04-28 23:52:29 +0200

    Basic structure

--- main.cpp
+++ main.cpp
@@ -2,42 +2,7 @@
 
 using namespace std;
 
-class Tdrzwi
-{
-    Tdrzwi() = delete;
-
-    int a;
-public:
-    Tdrzwi(int a)
-    {
-        this->a = a;
-    }
-};
-
-class Topona
-{
-    int z;
-public:
-    Topona(int i)
-    {
-        z = i;
-    }
-};
-
-class Tsamochod
-{
-    Tdrzwi lewe, prawe;
-    bool ifElectric;
-
-    Tsamochod() : lewe{1}, prawe{2}, ifElectric{false}
-    {
-        //dalesze ciało
-    }
-
-    Topona tablica[4]{Topona(1), Topona(2), Topona(3), Topona(4)};
-};
-
 int main()
 {
-
+    cout<<"xd";
 }
