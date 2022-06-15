fe1227ea0faabcdf159135427421b7f2c4e656d2
PiotrCiosmak <piotrciosmak2001@gmail.com>
2022-06-15 14:05:30 +0200

    R33 P14 added

---
+++ R33 - Operacje we wy - podstawy/P14 - Funkcja put/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   char napis[] = "Listopad";
+   int i=0;
+    do
+    {
+        cout.put(napis[i]).put('-');
+    }while(napis[i++]);
+}
